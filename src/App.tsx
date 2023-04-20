a82caad16b20a03d20de5da9de07a49b35661de2
Pawel Fryca <pawel.fryca@gmail.com>
2023-04-20 11:02:57 +0200

    Add sidebar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
-import { Button } from 'primereact/button';    
-import { InputText } from 'primereact/inputtext';     
-import './App.scss'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import { routes } from "./routes";
+import "./App.scss";
 
-import "primereact/resources/themes/md-light-indigo/theme.css";     
+import "primereact/resources/themes/md-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";                                       
-         
+import "primeicons/primeicons.css";
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+console.log(
+  "[App.tsx]",
+  `Hello world from Electron ${process.versions.electron}!`
+);
+
 
 function App() {
-  const [text, setText] = useState('')
-
   return (
-    <div className='App'>
-      <InputText value={text} onChange={e => setText(e.target.value)}></InputText>
-      <br></br>
-      <Button type='button' label='Submit' icon='pi pi-check'></Button>
-      <br></br>
-      {text}
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {routes}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './samples/node-api'
-import './index.scss'
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
 
